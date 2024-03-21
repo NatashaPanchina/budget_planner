1e960b15ba9c482d7a37ce54ec0b45ad933416f4
NatashaPanchina <panchinanata25@gmail.com>
2024-03-21 14:26:50 +0500

    firebase configs into env variables

--- src/configs/firebaseConfigs.js
+++ src/configs/firebaseConfigs.js
@@ -1,11 +1,11 @@
 const firebaseConfigs = {
-  apiKey: 'AIzaSyCAi4rgMJ9NLdDJRR7RWvr6pyr7JmIottI',
-  authDomain: 'neko-budget.firebaseapp.com',
-  projectId: 'neko-budget',
-  storageBucket: 'neko-budget.appspot.com',
-  messagingSenderId: '147123735960',
-  appId: '1:147123735960:web:c010c5de8a0fc9e218efed',
-  measurementId: 'G-YZYJDL8KRJ',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 export default firebaseConfigs;
